ae7c7b8ccfdadcce734b28e8b830208b0528cdce
4Bee <4bee.code@gmail.com>
2022-07-26 23:44:45 +0900

    12th commit

---
+++ Python_300Question/Q101_200/1pt.py
@@ -0,0 +1,95 @@
+# 101 ~ 110
+# 101
+# 파이썬에서 True 혹은 False를 갖는 데이터 타입은 무엇인가?
+# 정답확인
+
+from tkinter.tix import Tree
+
+
+print("boolean")
+
+# 102
+# 아래 코드의 출력 결과를 예상하라
+# print(3 == 5)
+# 정답확인
+
+print(False)
+
+# 103
+# 아래 코드의 출력 결과를 예상하라
+# print(3 < 5)
+# 정답확인
+
+print(True)
+
+# 104
+# 아래 코드의 결과를 예상하라.
+# x = 4
+# print(1 < x < 5)
+# 정답확인
+
+print(True)
+
+# 105
+# 아래 코드의 결과를 예상하라.
+# print ((3 == 3) and (4 != 3))
+# 정답확인
+
+print(True)
+
+# 106
+# 아래 코드에서 에러가 발생하는 원인에 대해 설명하라.
+# print(3 => 4)
+# 정답확인
+
+#My solution
+print(False)
+#solution
+print("지원하지 않는 연산자입니다. -> '<=','>=' ")
+
+
+# 107
+# 아래 코드의 출력 결과를 예상하라
+# if 4 < 3:
+#     print("Hello World")
+# 정답확인
+
+print("조건을 만족하지 않기 때문에 아무 결과도 출력되지 않습니다.")
+
+# 108
+# 아래 코드의 출력 결과를 예상하라
+# if 4 < 3:
+#     print("Hello World.")
+# else:
+#     print("Hi, there.")
+# 정답확인
+
+print("Hi, there.")
+
+# 109
+# 아래 코드의 출력 결과를 예상하라
+# if True :
+#     print ("1")
+#     print ("2")
+# else :
+#     print("3")
+# print("4")
+# 정답확인
+
+print('1,2,4')
+
+# 110
+# 아래 코드의 출력 결과를 예상하라
+
+# if True :
+#     if False:
+#         print("1")
+#         print("2")
+#     else:
+#         print("3")
+# else :
+#     print("4")
+# print("5")
+# 정답확인
+
+print('3,5')
