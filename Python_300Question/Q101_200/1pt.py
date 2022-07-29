e76cb16ea22d582ec27a82baadb5376df2259fcc
4Bee <4bee.code@gmail.com>
2022-07-29 17:05:35 +0900

    14th commit

--- Python_300Question/Q101_200/1pt.py
+++ Python_300Question/Q101_200/1pt.py
@@ -3,9 +3,6 @@
 # 파이썬에서 True 혹은 False를 갖는 데이터 타입은 무엇인가?
 # 정답확인
 
-from tkinter.tix import Tree
-
-
 print("boolean")
 
 # 102
--- Python_300Question/Q101_200/3pt.py
+++ Python_300Question/Q101_200/3pt.py
@@ -60,12 +60,20 @@
 # 정답확인
 
 #mysolution
-print('통화명\t환율')
-print('달러\t1167\n엔\t1.096\n유로\t1268\n위안\t171\n')
-user = input('입력: ')
-user.split(' ')
-print(list(user))
+# print('통화명\t환율')
+# money = '달러 1167 엔 1.096 유로 1268 위안 171'
+# user = input('입력: ')
+# money = money.split(' ')
+# print(money)
 
+#solution
+# 환율 = {"달러": 1167, 
+#         "엔": 1.096, 
+#         "유로": 1268, 
+#         "위안": 171}
+# user = input("입력: ")
+# num, currency = user.split()
+# print(float(num) * 환율[currency], "원")
 
 # 124
 # 사용자로부터 세 개의 숫자를 입력 받은 후 가장 큰 숫자를 출력하라.
@@ -74,6 +82,35 @@ print(list(user))
 # >> input number3: 20
 # 20
 # 정답확인
+
+# #mysolution
+# num1 = input('1st number is ')
+# num2 = input('2nd number is ')
+# num3 = input('3rd number is ')
+
+# num = num1,num2,num3
+# #num = list(num)
+# #print(num[0],type(num))
+# print('min = ',min(num))
+# print('max = ',max(num))
+# # for N in num :
+# #     if N < num[0]:
+
+# #solution
+# num1 = input("input number1: ")
+# num2 = input("input number2: ")
+# num3 = input("input number3: ")
+# num1 = int(num1)
+# num2 = int(num2)
+# num3 = int(num3)
+
+# if num1 >= num2 and num1 >= num3:
+#     print(num1)
+# elif num2 >= num1 and num2 >= num3:
+#     print(num2)
+# else:
+#     print(num3)
+
 
 # 125
 # 휴대폰 번호 앞자리에 따라 통신사는 아래와 같이 구분된다. 사용자로부터 휴대전화 번호를 입력 받고, 통신사를 출력하는 프로그램을 작성하라.
@@ -86,7 +123,18 @@ print(list(user))
 # 당신은 SKT 사용자입니다.
 # 정답확인
 
+# num = input('What is your number?\n(ex 011-0000-0000) :')
+# num = num.split('-')
+# print('inputed your number is ',num)
 
+# if num[0] == '011' :
+#     print('SKT')
+# elif num[0] == '016' :
+#     print('KT')
+# elif num[0] == '019' :
+#     print('LGU')
+# else :
+#     print('알수없음')
 
 # 126
 # 우편번호는 5자리로 구성되는데, 앞의 세자리는 구를 나타낸다. 예를들어, 강북구의 경우 010, 011, 012 세 자리로 시작한다.
@@ -99,7 +147,34 @@ print(list(user))
 # 도봉구
 # 정답확인
 
+# address = input('Aderess(max input number is 5) : ')
 
+# limit = len(address)
+# if int(limit) > 5 :
+#     print("잘못된 주소!!")
+# else :
+#     #print(int(address[2]))
+#     if int(address[2]) == 0 or int(address[2]) == 1 or int(address[2]) == 2:
+#         print('강북구')
+    
+#     elif int(address[2]) == 3 or int(address[2]) == 4 or int(address[2]) == 5:
+#         print('도봉구')
+    
+#     elif int(address[2]) == 6 or int(address[2]) == 7 or int(address[2]) == 8 or int(address[2]) == 9 :
+#         print('노원구')
+
+#왜 여기서는 int(address[2]) == 0 or 1 or 2: 에서 애러가 날까?
+# #print(limit)
+
+# #solution
+# 우편번호 = input("우편번호: ")
+# 우편번호 = 우편번호[:3]
+# if 우편번호 in ["010", "011", "012"]:
+#     print("강북구")
+# elif 우편번호 in ["014", "015", "016"]:
+#     print("도봉구")
+# else:
+#     print("노원구")
 
 # 127
 # 주민등록번호 뒷 자리 7자리 중 첫째 자리는 성별을 나타내는데, 1, 3은 남자 2, 4는 여자를 의미한다. 사용자로부터 13자리의 주민등록번호를 입력 받은 후 성별 (남자, 여자)를 출력하는 프로그램을 작성하라.
@@ -108,7 +183,15 @@ print(list(user))
 # 남자
 # 정답확인
 
-
+# id = input("id : ")
+# id = id.split('-')
+# # bid = id[1].split()
+# bid = list(id[1])
+# if bid[0] == '1' or'3':
+#     print('Male')
+# elif bid[0] == '2' or '4':
+#     print('Fmale')
+# print(bid)
 
 # 128
 # 주민등록번호의 뒷 자리 7자리 중 두번째와 세번째는 지역코드를 의미한다. 주민 등록 번호를 입력 받은 후 출생지가 서울인지 아닌지 판단하는 코드를 작성하라
@@ -122,6 +205,22 @@ print(list(user))
 # 서울 입니다.
 # 정답확인
 
+#mysolution
+# id = input("id : ")
+# id = id.split('-')[1]
+# # bid = 
+# if id[1:3] in ['00','01','02','03','04','05','06','07','08']:
+#     print('서울 입니다.')
+# else :
+#     print('서울이 아닙니다.')
+
+#solution
+# 주민번호 = input("주민등록번호: ")
+# 뒷자리 = 주민번호.split("-")[1]
+# if 0 <= int(뒷자리[1:3]) <= 8:
+#     print("서울입니다.")
+# else:
+#     print("서울이 아닙니다.")
 
 
 # 129
@@ -140,7 +239,10 @@ print(list(user))
 # 유효하지 않은 주민등록번호입니다. 
 # 정답확인
 
-
+# id = input("id : ")
+# #id = int(id)
+# id = id.split('-')
+# id = list(id[0]) + list(id[1])
 
 # 130
 # 아래 코드는 비트코인의 가격 정보를 딕셔너리로 가져오는 코드이다.
@@ -155,3 +257,11 @@ print(list(user))
 # min_price	최근 24시간 내 최저 거래금액
 # max_price	최근 24시간 내 최고 거래금액
 # 정답확인
+
+import requests
+btc = requests.get("https://api.bithumb.com/public/ticker/").json()['data']
+
+print(btc,type(btc))
+
+print('상승장')
+print('하락장')
