2194c7b2b60481d88a2155a1b94940e8e37f4b76
4Bee <4bee.code@gmail.com>
2022-08-07 23:53:21 +0900

    19th commit

--- my_test.py
+++ my_test.py
@@ -8,6 +8,9 @@
 # print("a = {} b = {}\n".format(a,b))
 #test
 
+from msvcrt import kbhit
+
+
 numbers = [(1, 2), (10, 0)]
 for a,b in numbers:
     print("a = {} b = {}".format(a,b))
@@ -25,3 +28,52 @@ for a, b in numbers:
         print("0으로 나눌 수는 없습니다.",b)
         continue
     print("{}을(를) {}로 나누면 {}".format(a, b, a/b))
+
+
+num = 5
+myn = 'ㄱ'
+pi_v = 3.14
+print("나는 연습을 %d번 했다." % num)
+print("나의 성씨 초성은 '%c'이다." % myn)
+print("나는 파이의 값이 %0.3f로 알고 있다." % pi_v)
+
+print("나는 연습을 %d번 했다. 나의 성씨 초성은 '%c'이다." % (num,  myn))
+
+a = [1,2,3,4]   #list
+result = []     #list
+
+for num in a:
+    result.append(num*3) #3,6,9,12
+print(result)
+
+print(type({})) # dict
+print(type(())) # tuple
+print(type([])) # list
+
+tmp = 0
+for i in range(0,11):
+    tmp += i
+    print(tmp)
+
+
+tmp = 0
+result=[]
+for i in range(0,11):
+    tmp += i
+    result.append(tmp)
+print(result)
+
+for my_list in [1,2,3,0] :
+    print(my_list)
+    if (my_list%2) == 0 :
+        print("조건에 해당됩니다. 중지합니다.")
+        break
+    print("before=",my_list)
+print("result=",my_list) #마지막 값이 나온다.
+
+a = 0
+while a < 10:
+    a = a + 1
+    if a % 2 == 0: 
+        continue
+    print(a)
