69b7f51fb59a7d0272e07d660b53b058410cd89e
4BFC <4bee.code@gmail.com>
2023-11-02 22:37:39 +0900

    new create median3a.py file

--- algo/Doit_Algorithm/median3a.py
+++ algo/Doit_Algorithm/median3a.py
@@ -1,19 +1,11 @@
-# 세 정수를 입력받아 중앙값 구하기1
+# 세 정수를 입력받아 중앙값 구하기2
 
-def med3(a, b, c,):
-    if a >= b:
-        if b >= c:
-            return b
-        elif a <= c:
-            return a
-        else:
-            return c
-    elif a > c:
+def med3(a, b, c):
+    if (b >= a and c <= a) or (b <= a and c >= a):
         return a
-    elif b > c:
-        return c
-    else:
+    elif (a > b and c < b) or (a < b and c > b):
         return b
+    return c
 
 
 print('세 정수의 중앙값을 구합니다.')
