6b6dc7f82c96a60bc3ef5709374a43a418295797
4BFC <4bee.code@gmail.com>
2024-01-02 20:52:23 +0900

    edited files

---
+++ algo/Baekjoon/00_다시풀기/구현/10988/10988.py
@@ -0,0 +1,7 @@
+N = list(input())
+n = N.copy()
+n.reverse()
+if n == N:
+    print(1)
+else:
+    print(0)
---
+++ algo/Baekjoon/6_정렬/23881.py
@@ -0,0 +1,39 @@
+n, k = map(int, input().split())
+
+N = list(input().split())
+
+cnt = 0
+
+for i in range(len(N)-1, -1, -1):
+    max = i
+    for ii in range(i-1, -1, -1):  # (len(N)-1, i, -1)
+        if N[max] < N[ii]:
+            cnt += 1
+            if cnt == k:
+                a, b = N[max], N[ii]
+            # if cnt == 4 and cnt > 4:
+            #     print(f'{N[max]},{N[ii]}')
+            # elif cnt != 4 and cnt < 4:
+            #     print(-1)
+            max = ii
+    N[max], N[i] = N[i], N[max]
+    # print(N)
+
+if cnt > k:
+    print(a, b)
+else:
+    print(-1)
+# print(N)
+# print(f"{N}, => {cnt}")
+
+
+# for i in range(len(N)):
+#     min = i
+#     print(N)
+#     for ii in range(i+1, len(N)):
+#         if N[min] > N[ii]:
+#             print(f'{N[i]}')
+#             min = ii
+#             cnt += 1
+#             # print(N[ii])
+#     N[min], N[i] = N[i], N[min]
