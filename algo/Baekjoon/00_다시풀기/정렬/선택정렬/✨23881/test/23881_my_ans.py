1bba06376e2b18e957ec77a10a0fce4129fe415f
4BFC <4bee.code@gmail.com>
2024-01-16 18:03:33 +0900

    update

---
+++ algo/Baekjoon/00_다시풀기/정렬/선택정렬/✨23881/test/23881_my_ans.py
@@ -0,0 +1,30 @@
+# n, k = map(int, input().split())
+# N = input().split()
+# cnt = 0
+import sys
+n, k = map(int, sys.stdin.readline().split())
+N = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+
+
+def selection(N):
+    global cnt
+
+    for last in range(len(N)-1, -1, -1):
+        max = last
+        for j in range(last, -1, -1):
+            if N[max] < N[j]:
+                max = j
+
+        if last != max:
+            cnt += 1
+            N[max], N[last] = N[last], N[max]
+            if cnt == k:
+                print(N[max], N[last])
+                exit()
+    print(-1)
+
+
+selection(N)
+# print(cnt)
+# print(N)
--- algo/my_Algo/Insertion_Sort/insertion_sort_2.py
+++ algo/my_Algo/Insertion_Sort/insertion_sort_2.py
@@ -1,14 +1,15 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(1, len(array)):
+    print(f"[{i}]")
     for j in range(i-1, 0, -1):  # 왜 역순인지 이해가 안감..
-        print(j)
+        # print(j)
         print(f"{array[j]} < {array[j-1]}")
         if array[j] < array[j-1]:  # i번째랑 바꾸면 기준인 i의 값과 혼동이 된다.
             array[j-1], array[j] = array[j], array[j-1]
         else:
             break
-    #     print(array)
+        print(array)
     # print(f"{array[i]} < {array[i-1]}")
 
 # print(array)
